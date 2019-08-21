1c10a6557861ebab21d9fc9b3694975010ceb845
academy_intern <academy_intern@academy-interns-MacBook-Air-3.local>
2019-08-21 17:10:37 +0200

    message

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+
+  imports: [
+  BrowserModule,
+  FormsModule
+]
+})
+
+export class AppModule { }
+
+
